1791012f16ce0bdf27086636edea9f1991133ffc
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 14:52:07 +0100

    Fix output message in HelloWorld.java to correct master designation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         //hello world
-<<<<<<< HEAD
-        System.out.println("je suis master !");
+        System.out.println("je suis masterclasse !");
         System.out.println("je suis le Nada !");
         System.out.println("je suis etudiante !");
-=======
-
-        System.out.println("je suis le test !");
->>>>>>> test
     }
 }
 
