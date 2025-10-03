c30048ec49a0d21210d9c5388265a6c19a361975
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 14:10:21 +0100

    Remove redundant output message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         //hello world
         System.out.println("je suis le Nada !");
-        System.out.println("je suis le Nada ii3 !");
     }
 }
 
