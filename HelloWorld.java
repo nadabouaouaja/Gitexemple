f334c97a4eb115a3e099300e45c7ffe646d4fc9a
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 14:54:55 +0100

    Update output message in HelloWorld.java to reflect correct master designation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         //saluutt
-        System.out.println("je suis master !");
+        System.out.println("je suis master000 !");
         System.out.println("je suis le Nada !");
         System.out.println("je suis etudiante !");
     }
