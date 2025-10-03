4132452896fe05557242ef1c5ac6c215486e8ddf
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 13:45:25 +0100

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         //hello world
-        System.out.println("Hello, World!");
+        System.out.println("je suis le master !");
 
 
 
