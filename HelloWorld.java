97d729232a820f60d20e0abb9bec3b77090a9d03
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 14:53:41 +0100

    Update output message in HelloWorld.java to include full designation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         //hello world
-        System.out.println("je suis masterclasse !");
+        System.out.println("je suis masterclasse5525 !");
         System.out.println("je suis le Nada !");
         System.out.println("je suis etudiante !");
     }
