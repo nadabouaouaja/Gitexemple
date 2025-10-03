d62e5320621187c35e8c63d5baffe6617856ae3c
nadabouaouaja <154356748+nadabouaouaja@users.noreply.github.com>
2025-10-03 14:00:06 +0100

    Update output messages in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         //hello world
-        System.out.println("je suis le master !");
-
-
-
-
+        System.out.println("je suis le Nada !");
+        System.out.println("je suis le Nada ii3 !");
     }
 }
 
